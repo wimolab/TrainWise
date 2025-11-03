cd21502f117017a7c98fe423af67395c91389b9d
LACHQER-Wiam <lachqerwiam@gmail.com>
2025-11-03 21:49:47 +0100

    manual implementation of faiss, I will use langchain in the future version

--- src/build_database.py
+++ src/build_database.py
@@ -1,48 +1,58 @@
+import os
+from turtle import pd
+
+from flask.cli import load_dotenv
 from utils_helpers import datacollection_utils as dcu
+from utils_helpers import database_management as dbm
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+import logging
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
 
 if __name__ == "__main__":
+    file_name = "./data/database/faiss_index.bin"
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    database = dbm.initialize_database(dimension=768)
+    faiss.write_index(database, file_name)
 
-    # url = "https://huggingface.co/blog/mlabonne/sft-llama3"
-    # html = dcu.scrape(url)
+    model = SentenceTransformer("nomic-ai/modernbert-embed-base")
 
-    # cleaned_text = dcu.clean_html_to_text(html)
-    # # print(cleaned_text)
+    load_dotenv()
+    sheet_id = os.getenv("GOOGLE_SHEETS_ID")
+    sheet_name = os.getenv("GOOGLE_SHEETS_NAME")
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    metadata = pd.read_csv(url)
 
-    # # 
-    # model = SentenceTransformer("nomic-ai/modernbert-embed-base")
+    not_read = []
+    for _, row in metadata.iterrows():
+        url = row["URL"]
+        if row["BaseFormat"] == "pdf":
+            try:
+                file_path = f"./data/input/{row['Id']}"
+                text = dcu.read_pdf_from_url(file_path=file_path)
+            except Exception as e:
+                not_read.append(url)
+                pass
 
+        if row["BaseFormat"] == "webpage":
+            try:
+                html = dcu.scrape(url)
+                text = dcu.clean_html_to_text(html)
+            except Exception as e:
+                not_read.append(url)
+                pass
 
-    # chunks = dcu.chunk_text(cleaned_text, chunk_size=400, overlap=80)
+        logging.info("The following URLs could not be read:\n%s", "\n".join(not_read))
 
-    # # Encoder les chunks (documents)
-    # doc_embeddings = model.encode([f"passage: {d}" for d in chunks], normalize_embeddings=True)
+        chunks = dcu.chunk_text(text, chunk_size=200, overlap=80)
+        doc_embeddings = model.encode([f"passage: {d}" for d in chunks], normalize_embeddings=True)
+        dbm.update_database(file_name, embeddings=doc_embeddings)
 
-    # # FAISS index
-    # dim = doc_embeddings.shape[1]
-    # index = faiss.IndexFlatIP(dim)
-    # index.add(doc_embeddings)
+        logging.info(f"All data was added to the Faiss Database")
 
-    # print(f"Index FAISS chargé avec {index.ntotal} embeddings.")
-
-    # ############################
-    # chunk_lookup = chunks
-
-    # query = "How should we do inference?"
-
-    # q_emb = model.encode([f"query: {query}"], normalize_embeddings=True)
-
-    # scores, ids = index.search(q_emb, k=3)
-
-    # for s, idx in zip(scores[0], ids[0]):
-    #     print(f"Score: {s:.3f}\nChunk: {chunk_lookup[idx]}\n")#[:200]
-
-
-
-    pdf_url = "https://arxiv.org/pdf/2408.13296v1"
-    save_path = f"./data/input/file1.pdf"
-    dcu.download_pdf(url=pdf_url, save_path=save_path)
-    text = dcu.read_pdf(save_path)
-    print(text)
+        
